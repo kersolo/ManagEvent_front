8dc1fd91d683d54a02fdea919daa68bef60f4016
kersolo <loick.ferelloc@gmail.com>
2024-03-14 15:45:12 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@material-tailwind/react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Manag'Event !!</h1>
+      <Button
+        placeholder={undefined}
+        children={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
+        Button
+      </Button>
+    </>
+  );
+}
+
+export default App;
