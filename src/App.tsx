f2b4a78b382d9d067a55457e573ad64f6a05906a
kersolo <loick.ferelloc@gmail.com>
2024-03-19 14:40:44 +0100

    add router before start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Welcome to Manag'Event !!</h1>
+      <Routes>
+        <Route path="" element="" />
+      </Routes>
     </>
   );
 }
