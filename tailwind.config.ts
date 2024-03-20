ffb18160cc6c48fc900196d3b1fd3e93856c72a0
Alextraterrestre <alexis.carlier07@gmail.com>
2024-03-20 17:30:54 +0100

    add backgroundImage in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,12 @@ import withMT from '@material-tailwind/react/utils/withMT';
 export default withMT({
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'mobileBgDp': "url('/src/assets/img/pictures/fireworkMobile.jpg')",
+        'desktopBgDp': "url('/src/assets/img/pictures/celebration-3443777.jpg')"
+      }
+    }
   },
   plugins: []
 });
