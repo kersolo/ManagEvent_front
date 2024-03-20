4ff1e936775c8681db33fe61bad4b165f863582d
MAUNICK JOSEPH MAYO <jomaytik@gmail.com>
2024-03-20 01:05:09 +0100

    EVENT_DEVPUNK-107

--- src/pages/Home/Homepage.tsx
+++ src/pages/Home/Homepage.tsx
@@ -5,21 +5,21 @@ import"./Homepage.css";
 export default function Homepage() {
     return (
     
-            <div className="text-white flex flex-col p-5 ">
+            <div className="flex flex-col p-5 ">
 
-                     <h1 className="mx-auto mt-20">Manag'Event</h1>
+                     <h1 className="mx-auto mt-20 text-white ">Manag'Event</h1>
                
-                     <p className="mt-20 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum neque in vel quod fuga illo sequi voluptate et voluptatibus dolor temporibus distinctio, expedita nam, eum soluta. Quaerat cupiditate, corporis quod iure pariatur sed animi quam aliquam, delectus esse assumenda incidunt quos voluptates in accusamus, nesciunt voluptate? Tenetur nobis fuga sapiente tempora similique quos odio hic voluptatibus debitis dicta corporis in, soluta, ut nisi beatae pariatur iusto sint omnis quam, aut autem! !</p>
+                     <p className="mt-20 text-justify text-white ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum neque in vel quod fuga illo sequi voluptate et voluptatibus dolor temporibus distinctio, expedita nam, eum soluta. Quaerat cupiditate, corporis quod iure pariatur sed animi quam aliquam, delectus esse assumenda incidunt quos voluptates in accusamus, nesciunt voluptate? Tenetur nobis fuga sapiente tempora similique quos odio hic voluptatibus debitis dicta corporis in, soluta, ut nisi beatae pariatur iusto sint omnis quam, aut autem! !</p>
             
               
-                    <Link to={"/Se-connecter"} className="mx-auto mt-20 ">
+                    <Link to={"/Se-connecter"} className="mx-auto mt-20 bg-yellow-500 mb-3 px-5 rounded">
                         Se connecter
                     </Link>
-                    <Link to={"/S'inscrire"} className="mx-auto">
+                    <Link to={"/S'inscrire"} className="mx-auto bg-yellow-500 px-9 rounded">
                         S'inscrire
                     </Link>
 
-                    <Link to={"/Contactez-nous"} className="mx-auto mt-20 border-b-2">
+                    <Link to={"/Contactez-nous"} className="mx-auto mt-20 border-b-2 text-white ">
                         <ContactUs />
                     </Link>
              
