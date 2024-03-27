67d4561bd77f13fe765567621fb847d7dded5c50
Alextraterrestre <alexis.carlier07@gmail.com>
2024-03-27 17:03:34 +0100

    add switched buttons between "notifs"&"messages & add fakes messages"

--- src/pages/Notification/NotificationPage.tsx
+++ src/pages/Notification/NotificationPage.tsx
@@ -3,48 +3,78 @@ import { useEffect, useState } from "react";
 import Notification from "../../components/Notifications";
 import NotiFaker from "./NotiFaker";
 import ButtonDefault from "../../components/ButtonDefault";
-
-
+import { Message } from "../../components/messageTest/messageTest";
+import messageFaker from "../../components/messageTest/messageFaker";
 
 export default function NotificationPage() {
     const [notifs, setNotifs] = useState(NotiFaker.notiFaker);
+    const [messages, setMessages] = useState(messageFaker.messageFaker);
+    const currentUrl = window.location.href;
+    const [showNotif, setShowNotif] = useState(false);
+    const [showMessage, setShowMessage] = useState(true);
 
-    const path = window.URL()
+    function handleClickNotif() {
+        console.log("clicked");
+        setShowNotif(true)
+        setShowMessage(false)
+    }
+    function handleClickMessage() {
+        console.log("clicked");
+        setShowNotif(false)
+        setShowMessage(true)
+    }
 
     useEffect(() => {
         console.log(NotiFaker.notiFaker);
         async function loadNotifications() {
             const Notifications = await NotiFaker.notiFaker
-            setNotifs(Notifications)
+            const Messages = await messageFaker.messageFaker
+            setNotifs(Notifications);
+            setMessages(Messages);
         }
-
         loadNotifications();
-
     }, [])
-    console.log(notifs);
-    return (
+
+    return (<>
         <div className="m-6 flex flex-col gap-4">
             <div className="h-12 flex flex-row justify-center gap-2">
-                {}
-                <ButtonDefault className="">Notifications</ButtonDefault>
                 <ButtonDefault
+                    onClick={handleClickMessage}
                     variant="secondary"
-                    className="border-lightBlueDP text-lightBlueDP ">
+                    className={showMessage ? "bg-orangeDP text-darkBlueDP hover:text-darkBlueDP" : "border-lightBlueDP text-lightBlueDP"}>
                     Messages
                 </ButtonDefault>
-            </div>
-            <div className="h-24">
+
+                {currentUrl ? <ButtonDefault
+                    onClick={handleClickNotif}
+                    variant="secondary"
+                    className={showNotif ? "bg-orangeDP text-darkBlueDP hover:text-darkBlueDP" : "border-lightBlueDP text-lightBlueDP"}>
+                    Notifications
+                </ButtonDefault>
+                    :
+                    <ButtonDefault className="">
+                        Notifications
+                    </ButtonDefault>}
 
             </div>
-            <div className="flex flex-col gap-4">
+            <div className="h-24 text-center">
+                Div de suppression
+            </div>
+
+
+            {showNotif && <div className="flex flex-col gap-4">
                 {notifs.length > 0 ? (notifs?.map((contenu, index) => (
                     <Notification element={contenu} key={index} />
                 ))) : <div className="border-2 border-orangeDP rounded-xl h-80 flex items-center justify-center">
                     <p className="font-semibold text-center text-2xl">Vous n'avez pas de notifications</p>
                 </div>}
-            </div>
+            </div>}
+            <div className="flex flex-col gap-4">
+            {showMessage && messages.map((contenu, index) => <Message element={contenu} key={index} />)}
+        </div>
         </div>
 
+    </>
 
     )
 }
