242c6df5593b4eb10f0e88d4657e8805217e5b79
Alextraterrestre <alexis.carlier07@gmail.com>
2024-03-27 11:36:17 +0100

    EVENT_DEVPUNK-88 finish notifs condition notifs ?

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -7,7 +7,7 @@ export default function Notification({ element }: NotifcationInterface) {
     return (
 
         <div
-        className="border-orangeDP border rounded-xl text-center flex items-center justify-center h-12">
+        className="border-orangeDP border-2 rounded-xl text-center flex items-center justify-center h-12">
             <p className="text-base">{element.content}</p>
         </div>)
 }
--- src/pages/Notification/NotificationPage.tsx
+++ src/pages/Notification/NotificationPage.tsx
@@ -9,7 +9,6 @@ import ButtonDefault from "../../components/ButtonDefault";
 export default function NotificationPage() {
     const [notifs, setNotifs] = useState(NotiFaker.notiFaker);
 
-
     useEffect(() => {
         console.log(NotiFaker.notiFaker);
         async function loadNotifications() {
@@ -24,18 +23,25 @@ export default function NotificationPage() {
     return (
         <div className="m-6 flex flex-col gap-4">
             <div className="h-12 flex flex-row justify-center gap-2">
-                <ButtonDefault className="w-1/2">Notifications</ButtonDefault>
-                <ButtonDefault variant="secondary" className="border-lightBlueDP text-lightBlueDP w-1/2">Messages</ButtonDefault>
+                <ButtonDefault className="">Notifications</ButtonDefault>
+                <ButtonDefault
+                    variant="secondary"
+                    className="border-lightBlueDP text-lightBlueDP ">
+                    Messages
+                </ButtonDefault>
             </div>
             <div className="h-24">
 
             </div>
-            <div className="flex flex-col gap-2">
-                {notifs.map((contenu, index) => (
+            <div className="flex flex-col gap-4">
+                {notifs.length > 0 ? (notifs?.map((contenu, index) => (
                     <Notification element={contenu} key={index} />
-                ))}
+                ))) : <div className="border-2 border-orangeDP rounded-xl h-80 flex items-center justify-center">
+                    <p className="font-semibold text-center text-2xl">Vous n'avez pas de notifications</p>
+                </div>}
             </div>
         </div>
+
 
     )
 }
