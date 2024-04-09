fd409be1fd36f9cca6fa728c188a36ed94763303
MAUNICK JOSEPH MAYO <jomaytik@gmail.com>
2024-04-09 19:18:49 +0200

    EVENT_DEVPUNK 57-CreateTask 54-TaskList

--- src/pages/Admin/HandleTask/TaskList.tsx
+++ src/pages/Admin/HandleTask/TaskList.tsx
@@ -8,15 +8,12 @@ import { useState } from "react";
 import "./TaskList.css";
 import CreateTask from "./CreateTask ";
 
-
 export interface taskList {
     nom: string,
     description: string
 }
 
 export default function TaskList(handleOpen, open) {
-
-   
 
     const [tasks, setTasks] = useState<taskList[]>([])
 
@@ -29,15 +26,15 @@ export default function TaskList(handleOpen, open) {
         <>
             <h1 className=" font-bold text-center text-xl mt-10">Espace Admin </h1>
             <h2 className=" font-bold text-center text-md mt-10">Liste des taches :</h2>
-            
+
             <div className=" flex justify-center  rounded-md mt-5 mb-5 relative">
-                <input type="text" placeholder="Recherche par nom" className="px-10 text-black"/>
-                <FontAwesomeIcon title="Voir la tache" icon={faSearch} className=" absolute text-black top-1 ms-60"/>
+                <input type="text" placeholder="Recherche par nom" className="px-10 text-black" />
+                <FontAwesomeIcon title="Voir la tache" icon={faSearch} className=" absolute text-black top-1 ms-60" />
             </div>
 
             <CreateTask onSubmit={onSubmit} handleOpen={handleOpen} open={open} />
-            {tasks.length ?( <div className="text-white mt-5">
-               
+            {tasks.length ? (<div className="text-white mt-5">
+
                 <div className="flex justify-center ">
                     <ul className=""> {tasks.map((task, index) => (
                         <li key={index} className="flex  mt-3 items-center  border border-orange-300 px-20 p-2 gap-20 rounded-lg" >
@@ -51,8 +48,8 @@ export default function TaskList(handleOpen, open) {
                     ))}
                     </ul>
                 </div>
-            </div>) :  (<p className="text-center text-md mt-10 text-orangeDP ">Aucune tache pour l'instant</p>)}
-    
+            </div>) : (<p className="text-center text-md mt-10 text-orangeDP ">Aucune tache pour l'instant</p>)}
+
         </>
     )
 }
