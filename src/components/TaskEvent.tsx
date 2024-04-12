c0c5f07172570ea79fb6440c22d941ddc1c58ef2
kersolo <loick.ferelloc@gmail.com>
2024-04-12 17:20:13 +0200

    EVENT_DEVPUNK-65 - Loick - Dialog unsubscribleEvent

--- src/components/TaskEvent.tsx
+++ src/components/TaskEvent.tsx
@@ -6,6 +6,8 @@ import EventDetail from './EventDetail';
 import { useEffect, useState } from 'react';
 import { get_user_task_event } from '../services/api/user_task_event';
 import { useNavigate, useParams } from 'react-router-dom';
+import Pencil from '../assets/pencil.svg';
+import { DialogUnsubscribeEvent } from './Dialog/DialogUnsubscribeEvent';
 
 export type TaskEventPropsType = {
   taskEvent: DetailEventPagePropsType;
@@ -18,6 +20,7 @@ export type UserTaskEventType = {
   user_id: number;
   task_event_id: number;
   status: string;
+  taskchoose: string;
 };
 
 export default function TaskEvent({
@@ -44,6 +47,11 @@ export default function TaskEvent({
     navigate('/events');
   };
 
+  const handleDelete = () => {
+    // delete_user_task_event(toParticipe?.user_id);
+    navigate('/events');
+  };
+
   return (
     <>
       <div className="flex flex-col gap-7 items-center text-center mt-8 w-3/4 m-auto ">
@@ -52,9 +60,16 @@ export default function TaskEvent({
             <EventDetail taskEvent={taskEvent} />
             <div>
               <p className="underline">Votre mission</p>
-              <p>La mission</p>
+              <div className="flex gap-3 mt-3 justify-center ">
+                <p>
+                  {users
+                    ?.filter((event) => event.user_id === toParticipe.user_id)
+                    .map((event) => event.taskchoose)}
+                </p>
+                <img src={Pencil} alt="" />
+              </div>
             </div>
-            <ButtonDefault variant="secondary">Se désinscrire</ButtonDefault>
+            <DialogUnsubscribeEvent handleDelete={handleDelete} />
             <ButtonDefault onClick={handleClick}>
               Retour à l'accueil
             </ButtonDefault>
@@ -103,64 +118,3 @@ export default function TaskEvent({
     </>
   );
 }
-// import { Typography } from '@material-tailwind/react';
-// import ButtonDefault from './ButtonDefault';
-// import { DetailEventPagePropsType } from '../pages/Events/DetailEventPage';
-// import RadioDefault from './RadioDefault';
-// import EventDetail from './EventDetail';
-//
-// export type TaskEventPropsType = {
-//   taskEvent: DetailEventPagePropsType;
-//   handleSubmit: (e: React.FormEvent) => void;
-//   handleChange: (evt: React.ChangeEvent<HTMLInputElement>) => void;
-//   value: number | null;
-// };
-//
-// export default function TaskEvent({
-//   taskEvent,
-//   handleSubmit,
-//   handleChange,
-//   value
-// }: TaskEventPropsType) {
-//   const { tasks, event } = taskEvent;
-//
-//   return (
-//     <>
-//       <div className="flex flex-col gap-7 items-center text-center mt-8 w-3/4 m-auto ">
-//         {event.status === 'open' && (
-//           <>
-//             <EventDetail taskEvent={taskEvent} />
-//             <form className="w-full" action="" onSubmit={handleSubmit}>
-//               <div className="mb-5 flex flex-col items-center">
-//                 <Typography className="underline font-normal mb-5" variant="h6">
-//                   Veuillez choisir une mission
-//                 </Typography>
-//                 <div>
-//                   {tasks.map((task, index) => (
-//                     <RadioDefault
-//                       key={index}
-//                       name="task"
-//                       value={task.id}
-//                       id={task.name}
-//                       checked={value === task.id}
-//                       onChange={handleChange}
-//                       label={task.name}
-//                       taskInfos={task}
-//                     />
-//                   ))}
-//                 </div>
-//               </div>
-//               <ButtonDefault type="submit">Je participe</ButtonDefault>
-//             </form>
-//           </>
-//         )}
-//         {event.status === 'close' && (
-//           <>
-//             <EventDetail taskEvent={taskEvent} />
-//             <ButtonDefault variant="disabled">Je participe</ButtonDefault>
-//           </>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
--- src/pages/Events/user_task_event_Faker.ts
+++ src/pages/Events/user_task_event_Faker.ts
@@ -2,6 +2,13 @@ const user_task_event_Faker = [
   {
     user_id: 1,
     task_event_id: 2,
+    taskchoose: 'buvette',
+    status: 'A venir'
+  },
+  {
+    user_id: 2,
+    task_event_id: 2,
+    taskchoose: 'stand crêpe',
     status: 'A venir'
   }
 ];
