e62bc87336b216c0dec3d02512632ad012a88e33
MAUNICK JOSEPH MAYO <jomaytik@gmail.com>
2024-06-20 15:59:14 +0200

    Message d'accueil, HOMEPAGE

--- src/pages/Home/Homepage.tsx
+++ src/pages/Home/Homepage.tsx
@@ -9,7 +9,7 @@ export default function Homepage() {
                 <div className="flex flex-col">
                     <h1 className="text-white mx-auto font-bold py-10">Manag'Event</h1>
                     <p className="text-justify text-white sm:w-10/12 sm:mx-auto  md:w-7/12 md:mx-auto lg:w-5/12 lg:mx-auto xl:w-4/12 xl:mx-auto py-2 p-2">
-                        Bienvenue sur Manag'Event, notre application dédiée à la célébration de la diversité des événements et des talents bénévoles. Chacune de ces occasions, qu'elles soient culturelles, caritatives, sportives ou artistiques, est une opportunité unique de rassembler nos forces et nos passions pour créer des expériences mémorables et uniques.
+                    Bienvenue sur Manag'Event, notre application dédiée à l'organisation d'événements variés et au recrutement de bénévoles. Chacune de ces occasions, qu'elles soient culturelles, caritatives, sportives ou artistiques, représente une formidable occasion de fusionner nos compétences et nos passions pour façonner des expériences inoubliables et uniques.
                     </p>
                 </div>
                 <div className="mb-5 mt-5">
