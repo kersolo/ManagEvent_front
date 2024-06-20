bfe3b55213c43bb57fe130f1e801c70ce937e70f
MAUNICK JOSEPH MAYO <jomaytik@gmail.com>
2024-06-20 15:54:10 +0200

    Message d'accueil HOMEPAGE

--- src/pages/Home/Homepage.tsx
+++ src/pages/Home/Homepage.tsx
@@ -3,41 +3,34 @@ import ButtonDefault from '../../components/ButtonDefault';
 import './Homepage.css';
 
 export default function Homepage() {
-  return (
-    <>
-      <div className="homepage pt-20 px-6">
-        <div className="flex flex-col">
-          <h1 className="text-white mx-auto font-bold py-10">Manag'Event</h1>
-          <p className="text-justify text-white sm:w-10/12 sm:mx-auto  md:w-7/12 md:mx-auto lg:w-5/12 lg:mx-auto xl:w-4/12 xl:mx-auto py-2 p-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            neque in vel quod fuga illo sequi voluptate et voluptatibus dolor
-            temporibus distinctio, expedita nam, eum soluta. Quaerat cupiditate,
-            corporis quod iure pariatur sed animi quam aliquam, delectus esse
-            assumenda incidunt quos voluptates in accusamus, nesciunt voluptate?
-            Tenetur nobis fuga sapiente tempora similique quos odio hic
-            voluptatibus debitis dicta corporis in, soluta, ut nisi beatae
-            pariatur iusto sint omnis quam, aut autem! !
-          </p>
-        </div>
-        <div className="mb-5 mt-5">
-          <div className="w-4/12  md:w-3/12 md:lg:w-3/12 lg:mx-auto xl:w-2/12  mb-2 mx-auto">
-            <Link to={'/login'}>
-              <ButtonDefault>Se connecter</ButtonDefault>
-            </Link>
-          </div>
-          <div className="w-4/12  md:w-3/12 lg:w-3/12  xl:w-2/12 mx-auto">
-            <Link to={'/register'}>
-              <ButtonDefault>S'inscrire</ButtonDefault>
-            </Link>
-          </div>
-        </div>
+    return (
+        <>
+            <div className="homepage pt-20 px-6">
+                <div className="flex flex-col">
+                    <h1 className="text-white mx-auto font-bold py-10">Manag'Event</h1>
+                    <p className="text-justify text-white sm:w-10/12 sm:mx-auto  md:w-7/12 md:mx-auto lg:w-5/12 lg:mx-auto xl:w-4/12 xl:mx-auto py-2 p-2">
+                        Bienvenue sur Manag'Event, notre application dédiée à la célébration de la diversité des événements et des talents bénévoles. Chacune de ces occasions, qu'elles soient culturelles, caritatives, sportives ou artistiques, est une opportunité unique de rassembler nos forces et nos passions pour créer des expériences mémorables et uniques.
+                    </p>
+                </div>
+                <div className="mb-5 mt-5">
+                    <div className="w-4/12  md:w-3/12 md:lg:w-3/12 lg:mx-auto xl:w-2/12  mb-2 mx-auto">
+                        <Link to={'/login'}>
+                            <ButtonDefault>Se connecter</ButtonDefault>
+                        </Link>
+                    </div>
+                    <div className="w-4/12  md:w-3/12 lg:w-3/12  xl:w-2/12 mx-auto">
+                        <Link to={'/register'}>
+                            <ButtonDefault>S'inscrire</ButtonDefault>
+                        </Link>
+                    </div>
+                </div>
 
-        <div className="flex justify-center mx-auto">
-          <Link to="/contact" className="border-b-2 text-white">
-            Contactez-nous
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+                <div className="flex justify-center mx-auto">
+                    <Link to="/contact" className="border-b-2 text-white">
+                        Contactez-nous
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
 }
